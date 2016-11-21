7d7f5f26fbed71248415af9270e0ba1df9c670eb
stevenwoah <stevenwoah@users.noreply.github.com>
2016-11-20 23:42:34 -0800

    Forgot to escape some money

--- packer/scripts/virtualbox-additions.sh
+++ packer/scripts/virtualbox-additions.sh
@@ -20,7 +20,7 @@ rm -f ${VBOX_ISO};
 
 # https://www.virtualbox.org/attachment/ticket/15695/mod_timer.patch
 cd ${VBOX_TMP};
-sed -i "358ised -i 's/\\\(#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 31)\\\)/\\\1 || LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)/g' ${INSTALLATION_DIR}/src/vboxguest-5.0.28/vboxguest/r0drv/linux/timer-r0drv-linux.c" install.sh;
+sed -i "358ised -i 's/\\\(#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 31)\\\)/\\\1 || LINUX_VERSION_CODE >= KERNEL_VERSION(4, 8, 0)/g' \${INSTALLATION_DIR}/src/vboxguest-5.0.28/vboxguest/r0drv/linux/timer-r0drv-linux.c" install.sh;
 sh install.sh;
 
 rm -rf ${VBOX_TMP};
